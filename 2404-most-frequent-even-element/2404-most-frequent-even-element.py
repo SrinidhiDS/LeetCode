577febc1432dbb2b2ddead9f8114d5924475498e
Srinidhi Shanmugam <110234137+SrinidhiDS@users.noreply.github.com>
2022-09-21 09:58:15 +0530

    Time: 764 ms (31.86%), Space: 14.4 MB (25.41%) - LeetHub

---
+++ 2404-most-frequent-even-element/2404-most-frequent-even-element.py
@@ -0,0 +1,22 @@
+class Solution:
+    def mostFrequentEven(self, nums: List[int]) -> int:
+        nums_count = {}
+        ans = []
+        for i in nums:
+            if i%2 == 0:
+                nums_count[i] = nums_count.get(i,0) + 1
+        if nums_count:
+            max_value = max(nums_count.items(),key = lambda x:x[1])
+            for k,v in nums_count.items():
+                if v == max_value[1]:
+                    ans.append(k)     
+            
+        
+            if len(ans) > 1:
+                return min(ans)
+            else:
+                for i in ans:
+                    return i 
+        else:
+            return -1
+    
